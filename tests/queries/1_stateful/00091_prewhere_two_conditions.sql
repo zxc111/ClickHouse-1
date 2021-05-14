88290017bc64a6a9727cf4432eca995e4ce874ce
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2021-05-14 10:20:22 +0300

    stateful

--- tests/queries/1_stateful/00091_prewhere_two_conditions.sql
+++ tests/queries/1_stateful/00091_prewhere_two_conditions.sql
@@ -2,12 +2,12 @@ SET max_bytes_to_read = 600000000;
 
 SET optimize_move_to_prewhere = 1;
 
-SELECT uniq(URL) FROM test.hits WHERE EventTime >= '2014-03-20 00:00:00' AND EventTime < '2014-03-21 00:00:00';
-SELECT uniq(URL) FROM test.hits WHERE EventTime >= '2014-03-20 00:00:00' AND URL != '' AND EventTime < '2014-03-21 00:00:00';
-SELECT uniq(*) FROM test.hits WHERE EventTime >= '2014-03-20 00:00:00' AND EventTime < '2014-03-21 00:00:00' AND EventDate = '2014-03-21';
-WITH EventTime AS xyz SELECT uniq(*) FROM test.hits WHERE xyz >= '2014-03-20 00:00:00' AND xyz < '2014-03-21 00:00:00' AND EventDate = '2014-03-21';
+SELECT uniq(URL) FROM test.hits WHERE EventTime >= toDateTime('2014-03-20 00:00:00', 'Europe/Moscow') AND EventTime < toDateTime('2014-03-21 00:00:00', 'Europe/Moscow');
+SELECT uniq(URL) FROM test.hits WHERE EventTime >= toDateTime('2014-03-20 00:00:00', 'Europe/Moscow') AND URL != '' AND EventTime < toDateTime('2014-03-21 00:00:00', 'Europe/Moscow');
+SELECT uniq(*) FROM test.hits WHERE EventTime >= toDateTime('2014-03-20 00:00:00', 'Europe/Moscow') AND EventTime < toDateTime('2014-03-21 00:00:00', 'Europe/Moscow') AND EventDate = '2014-03-21';
+WITH EventTime AS xyz SELECT uniq(*) FROM test.hits WHERE xyz >= toDateTime('2014-03-20 00:00:00', 'Europe/Moscow') AND xyz < toDateTime('2014-03-21 00:00:00', 'Europe/Moscow') AND EventDate = '2014-03-21';
 
 SET optimize_move_to_prewhere = 0;
 
-SELECT uniq(URL) FROM test.hits WHERE EventTime >= '2014-03-20 00:00:00' AND EventTime < '2014-03-21 00:00:00'; -- { serverError 307 }
-SELECT uniq(URL) FROM test.hits WHERE EventTime >= '2014-03-20 00:00:00' AND URL != '' AND EventTime < '2014-03-21 00:00:00'; -- { serverError 307 }
+SELECT uniq(URL) FROM test.hits WHERE EventTime >= toDateTime('2014-03-20 00:00:00', 'Europe/Moscow') AND EventTime < toDateTime('2014-03-21 00:00:00', 'Europe/Moscow'); -- { serverError 307 }
+SELECT uniq(URL) FROM test.hits WHERE EventTime >= toDateTime('2014-03-20 00:00:00', 'Europe/Moscow') AND URL != '' AND EventTime < toDateTime('2014-03-21 00:00:00', 'Europe/Moscow'); -- { serverError 307 }
